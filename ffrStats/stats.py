ca5e17f4c0897c449fea63d2e3966577fb0c96f5
Spencer Spenst <spencerspenst@gmail.com>
2019-05-19 10:52:38 -0700

    spaces instead of tabs

--- ffrStats/stats.py
+++ ffrStats/stats.py
@@ -20,81 +20,81 @@ import sys
 output_file = 'level_ranks.txt'
 
 def get_data(credentials, stats_username):
-	"""
-	Given a credentials object, returns all level rank data for 'stats_username'.
-	"""
-	url_base = 'http://www.flashflashrevolution.com'
-	url_levelrank = url_base + '/levelrank.php?sub=' + stats_username
+    """
+    Given a credentials object, returns all level rank data for 'stats_username'.
+    """
+    url_base = 'http://www.flashflashrevolution.com'
+    url_levelrank = url_base + '/levelrank.php?sub=' + stats_username
 
-	# initialize mechanize browser
-	br = mechanize.Browser()
-	br.set_handle_robots(False)
-	br.addheaders = [('User-agent', 'Mozilla/5.0')] 
+    # initialize mechanize browser
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
+    br.addheaders = [('User-agent', 'Mozilla/5.0')] 
 
-	print('[+] GET ' + url_base)
-	br.open(url_base)
+    print('[+] GET ' + url_base)
+    br.open(url_base)
 
-	print('[+] Logging in with credentials...')
-	br.form = br.forms()[0]
-	br.form['vb_login_username'] = credentials['username']
-	br.form['vb_login_password'] = credentials['password']
-	br.submit()
+    print('[+] Logging in with credentials...')
+    br.form = br.forms()[0]
+    br.form['vb_login_username'] = credentials['username']
+    br.form['vb_login_password'] = credentials['password']
+    br.submit()
 
-	print('[+] GET ' + url_levelrank)
-	data = br.open(url_levelrank)
-	# print(data.read())
+    print('[+] GET ' + url_levelrank)
+    data = br.open(url_levelrank)
+    # print(data.read())
 
-	soup = BeautifulSoup(data, 'html.parser', from_encoding='iso-8859-1')
+    soup = BeautifulSoup(data, 'html.parser', from_encoding='iso-8859-1')
 
-	if soup.table == None:
-		print("[+] ERROR: Invalid login credentials")
-		exit()
+    if soup.table == None:
+        print("[+] ERROR: Invalid login credentials")
+        exit()
 
-	return soup.table
+    return soup.table
 
 def format_data(raw_data):
-	"""
-	Given raw level rank data, returns a list of OrderedDict objects containing the
-	difficulty, rank, and name of all levels that have not been AAA'd.
-	"""
+    """
+    Given raw level rank data, returns a list of OrderedDict objects containing the
+    difficulty, rank, and name of all levels that have not been AAA'd.
+    """
 
-	print('[+] Formatting level ranks...')
+    print('[+] Formatting level ranks...')
 
-	# get table columns
-	cols = {}
-	for i, th in enumerate(raw_data('tr')[0]('th')):
-		if th.span:
-			# remove arrow from table headers
-			th.span.extract()
-		cols[th.string] = i
+    # get table columns
+    cols = {}
+    for i, th in enumerate(raw_data('tr')[0]('th')):
+        if th.span:
+            # remove arrow from table headers
+            th.span.extract()
+        cols[th.string] = i
 
-	levels = []
+    levels = []
 
-	for tr in raw_data('tr')[1:]:
-		row = tr('td')
-		# skip entries that are already AAA'd
-		rank = int(row[cols['Rank']].string.replace(',', ''))
-		if rank != 1:
-			# extract meaningful data for each level
-			difficulty = int(row[cols['D']].string)
-			name = row[cols['Level']].string
+    for tr in raw_data('tr')[1:]:
+        row = tr('td')
+        # skip entries that are already AAA'd
+        rank = int(row[cols['Rank']].string.replace(',', ''))
+        if rank != 1:
+            # extract meaningful data for each level
+            difficulty = int(row[cols['D']].string)
+            name = row[cols['Level']].string
 
-			levels.append(OrderedDict([('D', difficulty),
-				('Rank', rank),
-				('Name', name)]))
+            levels.append(OrderedDict([('D', difficulty),
+                ('Rank', rank),
+                ('Name', name)]))
 
-	return levels
+    return levels
 
 credentials = json.loads(open('credentials', 'r').read())
 
 # get the username that the stats will be retrieved for
 stats_username = credentials['username']
 if len(sys.argv) == 2:
-	stats_username = sys.argv[1]
+    stats_username = sys.argv[1]
 elif len(sys.argv) != 1:
-	print('Invalid argument format. Please call this script with the following format:')
-	print('\tpython3 stats.py <OPTIONAL:stats_username>')
-	sys.exit()
+    print('Invalid argument format. Please call this script with the following format:')
+    print('\tpython3 stats.py <OPTIONAL:stats_username>')
+    sys.exit()
 
 raw_data = get_data(credentials, stats_username)
 levels = format_data(raw_data)
