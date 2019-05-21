668decce62210f0eacc258afdec9fdb72fb0ccd5
Spencer Spenst <spencerspenst@gmail.com>
2019-05-20 22:45:32 -0700

    per-level or per-difficulty stats

--- ffrStats/stats.py
+++ ffrStats/stats.py
@@ -19,6 +19,27 @@ import sys
 
 output_file = 'level_ranks.txt'
 
+difficulties = [
+    ('Brutal', 100),
+    ('Guru', 85),
+    ('Master', 70),
+    ('Very Challenging', 60),
+    ('Challenging', 50),
+    ('Very Difficult', 42),
+    ('Difficult', 34),
+    ('Tricky', 26),
+    ('Standard', 18),
+    ('Easy', 11),
+    ('Very Easy', 7),
+    ('Beginner', 4),
+    ('Easiest', 1)
+]
+
+def get_difficulty_index(d):
+    for i in range(len(difficulties)):
+        if d >= difficulties[i][1]:
+            return i
+
 def get_data(credentials, stats_username):
     """
     Given a credentials object, returns all level rank data for 'stats_username'.
@@ -52,6 +73,13 @@ def get_data(credentials, stats_username):
 
     return soup.table
 
+class Totals:
+    """Class to keep track of scores"""
+    def __init__(self):
+        self.total = 0
+        self.aaa = 0
+        self.fc = 0
+
 def format_data(raw_data):
     """
     Given raw level rank data, returns a list of OrderedDict objects containing the
@@ -70,18 +98,44 @@ def format_data(raw_data):
 
     levels = []
 
+    # per-difficulty distribution
+    dd = [Totals() for _ in range(len(difficulties))]
+    # per-level distribution
+    ld = {}
+
     for tr in raw_data('tr')[1:]:
         row = tr('td')
-        # skip entries that are already AAA'd
         rank = int(row[cols['Rank']].string.replace(',', ''))
+        difficulty = int(row[cols['D']].string)
+
+        index = get_difficulty_index(difficulty)
+        dd[index].total += 1
+
+        if difficulty not in ld:
+            ld[difficulty] = Totals()
+
+        ld[difficulty].total += 1
+
         if rank != 1:
-            # extract meaningful data for each level
-            difficulty = int(row[cols['D']].string)
+            # keep track of levels that are already AAA'd
             name = row[cols['Level']].string
 
             levels.append(OrderedDict([('D', difficulty),
                 ('Rank', rank),
                 ('Name', name)]))
+        else:
+            dd[index].aaa += 1
+            ld[difficulty].aaa += 1
+
+        if row[cols['P']].string == row[cols['C']].string:
+            dd[index].fc += 1
+            ld[difficulty].fc += 1
+
+    print(tabulate([[difficulties[i], dd[i].aaa, dd[i].fc, dd[i].total] for i in range(len(difficulties)-1, -1, -1)],
+        headers=['Difficulty', 'AAAs', 'FCs', 'Total']))
+
+    print(tabulate(sorted([[d, ld[d].aaa, ld[d].fc, ld[d].total] for d in ld], key=lambda x:x[0]),
+        headers=['D', 'AAAs', 'FCs', 'Total']))
 
     return levels
 
