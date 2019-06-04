9b81bd7d586465a0f21487cd34bc866a9273ab8f
Spencer Spenst <spencerspenst@gmail.com>
2019-06-03 20:06:40 -0700

    stats are auto-posted to my page; unique output filenames

--- ffrStats/stats.py
+++ ffrStats/stats.py
@@ -16,8 +16,7 @@ import json
 import mechanize
 import re
 import sys
-
-output_file = 'level_ranks.txt'
+import time
 
 difficulties = [
     ('Brutal', 100),
@@ -54,14 +53,14 @@ class Tier:
         self.sum = 0
         self.perfect = 0
 
-def printAAAsAndFCs(totals):
+def printAAAsAndFCs(totals, f):
     if totals.aaa != totals.total and totals.aaa != 0:
-        print(' %d/%d AAAs' % (totals.aaa, totals.total), end='')
+        f.write(' %d/%d [color=#D95819]AAAs[/color]' % (totals.aaa, totals.total))
     if totals.fc != totals.total:
-        print(' %d/%d FCs' % (totals.fc, totals.total), end='')
-    print()
+        f.write(' %d/%d [color=#009900]FCs[/color]' % (totals.fc, totals.total))
+    f.write('\n')
 
-def format_data(raw_data):
+def format_data(raw_data, output_filename):
     """
     Given raw level rank data, returns a list of OrderedDict objects containing the
     difficulty, rank, and name of all levels that have not been AAA'd.
@@ -97,6 +96,7 @@ def format_data(raw_data):
 
         ld[difficulty].total += 1
 
+        # TODO: rank 1 != AAA !!!!! almost always but pls make it accurate
         if rank != 1:
             # keep track of levels that are already AAA'd
             name = row[cols['Level']].string
@@ -115,19 +115,22 @@ def format_data(raw_data):
     # print(tabulate([[difficulties[i], dd[i].aaa, dd[i].fc, dd[i].total] for i in range(len(difficulties)-1, -1, -1) if dd[i].aaa != dd[i].total],
     #     headers=['Difficulty', 'AAAs', 'FCs', 'Total']))
 
-    for i in range(len(difficulties)-1, -1, -1):
-        if dd[i].aaa == dd[i].total:
-            continue
-        print('%s:' % difficulties[i][0], end='')
-        printAAAsAndFCs(dd[i])
+    with open(output_filename, 'w') as f:
+        for i in range(len(difficulties)-1, -1, -1):
+            if dd[i].aaa == dd[i].total:
+                continue
+            f.write('%s:' % difficulties[i][0])
+            printAAAsAndFCs(dd[i], f)
 
-    for d, t in sorted(ld.items(), key=lambda x:x[0]):
-        if t.aaa == t.total:
-            continue
-        print('Level %d:' % d, end='')
-        printAAAsAndFCs(t)
+        f.write('\n')
 
-    return levels
+        for d, t in sorted(ld.items(), key=lambda x:x[0]):
+            if t.aaa == t.total:
+                continue
+            f.write('[color=#FF9900]%d[/color]:' % d)
+            printAAAsAndFCs(t, f)
+
+    # return levels
 
 credentials = json.loads(open('credentials', 'r').read())
 
@@ -144,6 +147,8 @@ elif len(sys.argv) != 1:
 url_base = 'http://www.flashflashrevolution.com'
 url_levelrank = url_base + '/levelrank.php?sub=' + stats_username
 url_tiers = url_base + '/FFRStats/level_tiers.php'
+url_post = url_base + '/profile/edit/thoughts/234639'
+# TODO: provide an option for which random thought is used, and throw an error if the thought is invalid
 
 def get_browser():
     """initialize mechanize browser"""
@@ -157,12 +162,21 @@ def login(br):
     br.open(url_base)
 
     print('[+] Logging in with credentials...')
-    br.form = br.forms()[0]
+    br.select_form(nr=0)
     br.form['vb_login_username'] = credentials['username']
     br.form['vb_login_password'] = credentials['password']
     br.submit()
 
     # TODO: check if credentials are correct here rather than throwing an error later on
+    # also "Logged in successful / unsuccessful print statements"
+
+def post(br, body):
+    print('[+] GET ' + url_post)
+    br.open(url_post)
+    br.select_form(nr=0)
+    br.form['blog_title'] = 'Stats'
+    br.form['blog_post'] = body
+    br.submit()
 
 br = get_browser()
 login(br)
@@ -175,17 +189,24 @@ if soup.table == None:
     print("[+] ERROR: Invalid login credentials")
     exit()
 
-levels = format_data(soup.table)
+output_filename = time.strftime("stats-%Y-%m-%d-%H-%M-%S.txt")
+
+format_data(soup.table, output_filename)
+
+# levels = format_data(soup.table, output_filename)
 
 # sort the level data primarily by difficulty, and secondly by lowest rank
-sorted_levels = sorted(levels, key=lambda level:level['Rank'])
-sorted_levels = sorted(sorted_levels, key=lambda level:level['D'])
+# sorted_levels = sorted(levels, key=lambda level:level['Rank'])
+# sorted_levels = sorted(sorted_levels, key=lambda level:level['D'])
 
-with open(output_file, 'w') as f:
-    f.write(tabulate(sorted_levels, headers='keys', tablefmt='simple'))
+# with open(output_filename, 'w') as f:
+#     f.write(tabulate(sorted_levels, headers='keys', tablefmt='simple'))
 
-print('[+] Level ranks written to ' + output_file)
+print('[+] Stats written to ' + output_filename)
 
+post(br, open(output_filename, 'r').read())
+
+print('[+] Stats posted to ' + output_filename)
 
 ##### TIERS #####
 
