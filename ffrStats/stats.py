98351ff0dd08f69d5713e8528b6d4110e049e614
Spencer Spenst <spencerspenst@gmail.com>
2019-06-07 00:04:32 -0700

    add token stats; some refactoring

--- ffrStats/stats.py
+++ ffrStats/stats.py
@@ -6,7 +6,7 @@ need to be AAA'd appear first.
 
 Assumes there is an existing file in this directory named 'credentials'
 that contains a username and password in JSON format:
-{"username":"YOUR_USERNAME","password":"YOUR_PASSWORD"}
+{'username':'YOUR_USERNAME','password':'YOUR_PASSWORD'}
 """
 
 from bs4 import BeautifulSoup
@@ -31,7 +31,8 @@ difficulties = [
     ('Easy', 11),
     ('Very Easy', 7),
     ('Beginner', 4),
-    ('Easiest', 1)
+    ('Easiest', 1),
+    ('Zero', 0)
 ]
 
 def get_difficulty_index(d):
@@ -45,6 +46,13 @@ class Totals:
         self.total = 0
         self.aaa = 0
         self.fc = 0
+
+    def add_levelrank(self, levelrank):
+        self.total += 1
+        if levelrank.fc:
+            self.fc += 1
+        if levelrank.isAAA():
+            self.aaa += 1
 
 class Tier:
     """Class to keep track of per-tier_total data"""
@@ -69,8 +77,12 @@ class Levelrank:
         self.c = int(row[cols['c']].string.replace(',', ''))
         self.played = int(row[cols['played']].string.replace(',', ''))
 
+    def isAAA(self):
+        return self.fc and self.p == self.c and self.b == 0
+
 def printAAAsAndFCs(totals, f):
-    if totals.aaa != totals.total and totals.aaa != 0:
+    # TODO: options for colors
+    if totals.aaa != totals.total and (totals.aaa != 0 or totals.fc == totals.total):
         f.write(' %d/%d [color=#D95819]AAAs[/color]' % (totals.aaa, totals.total))
     if totals.fc != totals.total:
         f.write(' %d/%d [color=#009900]FCs[/color]' % (totals.fc, totals.total))
@@ -87,44 +99,36 @@ def extract_levelranks(raw_data):
 
     return [Levelrank(tr('td'), cols) for tr in raw_data('tr')[1:]]
 
-def format_data(levelranks, output_filename):
+def format_data(levelranks, output_filename, title, write_ld):
     # per-difficulty distribution
     dd = [Totals() for _ in range(len(difficulties))]
     # per-level distribution
     ld = {}
 
     for levelrank in levelranks:
-        dd_totals = dd[get_difficulty_index(levelrank.d)]
+        dd[get_difficulty_index(levelrank.d)].add_levelrank(levelrank)
         if levelrank.d not in ld:
             ld[levelrank.d] = Totals()
-        ld_totals = ld[levelrank.d]
+        ld[levelrank.d].add_levelrank(levelrank)
 
-        dd_totals.total += 1
-        ld_totals.total += 1
-
-        if levelrank.fc:
-            dd_totals.fc += 1
-            ld_totals.fc += 1
-            if levelrank.p == levelrank.c and levelrank.b == 0:
-                dd_totals.aaa += 1
-                ld_totals.aaa += 1
-
-    with open(output_filename, 'w') as f:
-        f.write('[b]Public Level Stats[/b]\n\n')
+    # TODO: probably move this writing part out of this function, or refactor this somehow
+    with open(output_filename, 'a') as f:
+        f.write('[b][u]' + title + '[/u][/b]\n\n')
 
         for i in range(len(difficulties)-1, -1, -1):
             if dd[i].aaa == dd[i].total:
                 continue
             f.write('[color=#FF9900]%s[/color]:' % difficulties[i][0])
             printAAAsAndFCs(dd[i], f)
-
         f.write('\n')
 
-        for d, t in sorted(ld.items(), key=lambda x:x[0]):
-            if t.aaa == t.total:
-                continue
-            f.write('[color=#FF9900]%d[/color]:' % d)
-            printAAAsAndFCs(t, f)
+        if (write_ld):
+            for d, t in sorted(ld.items(), key=lambda x:x[0]):
+                if t.aaa == t.total:
+                    continue
+                f.write('[color=#FF9900]%d[/color]:' % d)
+                printAAAsAndFCs(t, f)
+            f.write('\n')
 
 credentials = json.loads(open('credentials', 'r').read())
 
@@ -137,87 +141,72 @@ elif len(sys.argv) != 1:
     print('\tpython3 stats.py <OPTIONAL:stats_username>')
     sys.exit()
 
+# TODO: provide an option for which random thought is used, and throw an error if the thought is invalid?
 random_thought_id = '234639'
 
 # URLs
 url_base = 'http://www.flashflashrevolution.com'
 url_levelrank = url_base + '/levelrank.php?sub=' + stats_username
+url_tokenlevelrank = url_base + '/levelrank_special.php?sub=' + stats_username
 url_tiers = url_base + '/FFRStats/level_tiers.php'
 url_post = url_base + '/profile/edit/thoughts/' + random_thought_id
-# TODO: provide an option for which random thought is used, and throw an error if the thought is invalid
 
-def get_browser():
-    """initialize mechanize browser"""
-    br = mechanize.Browser()
-    br.set_handle_robots(False)
-    br.addheaders = [('User-agent', 'Mozilla/5.0')]
-    return br
+class Browser:
+    def __init__(self):
+        """Initialize mechanize browser"""
+        self.br = mechanize.Browser()
+        self.br.set_handle_robots(False)
+        self.br.addheaders = [('User-agent', 'Mozilla/5.0')]
 
-def login(br):
-    print('[+] GET ' + url_base)
-    br.open(url_base)
+    def login(self):
+        print('[+] GET ' + url_base)
+        self.br.open(url_base)
 
-    print('[+] Logging in with credentials...')
-    br.select_form(nr=0)
-    br.form['vb_login_username'] = credentials['username']
-    br.form['vb_login_password'] = credentials['password']
-    br.submit()
+        print('[+] Logging in with credentials...')
+        self.br.select_form(nr=0)
+        self.br.form['vb_login_username'] = credentials['username']
+        self.br.form['vb_login_password'] = credentials['password']
+        self.br.submit()
 
-    # TODO: check if credentials are correct here rather than throwing an error later on
-    # also "Logged in successful / unsuccessful print statements"
+        # TODO: check if credentials are correct here rather than throwing an error later on
+        # also "Logged in successful / unsuccessful print statements"
 
-def post(br, body):
-    print('[+] GET ' + url_post)
-    br.open(url_post)
-    br.select_form(nr=0)
-    br.form['blog_title'] = 'Stats'
-    br.form['blog_post'] = body
-    br.submit()
+    def get(self, url):
+        print('[+] GET ' + url)
+        data = self.br.open(url)
+        return BeautifulSoup(data, 'html.parser', from_encoding='iso-8859-1')
 
-br = get_browser()
-login(br)
+    def post_stats(self, body):
+        print('[+] GET ' + url_post)
+        self.br.open(url_post)
+        self.br.select_form(nr=0)
+        self.br.form['blog_title'] = 'Stats'
+        self.br.form['blog_post'] = body
+        self.br.submit()
+        print('[+] Stats posted to ' + random_thought_id)
 
-print('[+] GET ' + url_levelrank)
-data = br.open(url_levelrank)
-soup = BeautifulSoup(data, 'html.parser', from_encoding='iso-8859-1')
+br = Browser()
+br.login()
 
-if soup.table == None:
-    print("[+] ERROR: Invalid login credentials")
-    exit()
+output_filename = time.strftime('stats-%Y-%m-%d-%H-%M-%S.txt')
+print('[+] Writing stats to ' + output_filename)
 
-levelranks = extract_levelranks(soup.table)
+levelranks = extract_levelranks(br.get(url_levelrank))
+format_data(levelranks, output_filename, 'Public Level Stats', True)
 
-output_filename = time.strftime("stats-%Y-%m-%d-%H-%M-%S.txt")
+tokenlevelranks = extract_levelranks(br.get(url_tokenlevelrank))
+format_data(tokenlevelranks, output_filename, 'Token Level Stats', False)
 
-format_data(levelranks, output_filename)
-
-# levels = format_data(soup.table, output_filename)
-
-# sort the level data primarily by difficulty, and secondly by lowest rank
-# sorted_levels = sorted(levels, key=lambda level:level['Rank'])
-# sorted_levels = sorted(sorted_levels, key=lambda level:level['D'])
-
-# with open(output_filename, 'w') as f:
-#     f.write(tabulate(sorted_levels, headers='keys', tablefmt='simple'))
-
-print('[+] Stats written to ' + output_filename)
-
-post(br, open(output_filename, 'r').read())
-
-print('[+] Stats posted to ' + random_thought_id)
+br.post_stats(open(output_filename, 'r').read())
 
 ##### TIERS #####
 
-print('[+] GET ' + url_tiers)
-data = br.open(url_tiers)
-soup = BeautifulSoup(data, 'html.parser', from_encoding='iso-8859-1')
-
-tier_mains = soup.find_all('div', class_="tier_main")
+tier_mains = br.get(url_tiers).find_all('div', class_='tier_main')
 tiers = {}
 
 for tier_main in tier_mains:
-    tier_earned = int(tier_main.find_all('span', class_="tier_earned")[0].string)
-    tier_total = int(tier_main.find_all('span', class_="tier_total")[0].string)
+    tier_earned = int(tier_main.find_all('span', class_='tier_earned')[0].string)
+    tier_total = int(tier_main.find_all('span', class_='tier_total')[0].string)
 
     if tier_total not in tiers:
         tiers[tier_total] = Tier()
